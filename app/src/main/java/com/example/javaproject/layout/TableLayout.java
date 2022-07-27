e591a77f111e9cfcb29b3a302c7ee083531b61e4
KHANHVM\KhanhVM <khanhvm112@gmail.com>
2022-07-27 14:53:00 +0700

    first commit

---
+++ app/src/main/java/com/example/javaproject/layout/TableLayout.java
@@ -0,0 +1,16 @@
+package com.example.javaproject.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.javaproject.R;
+
+public class TableLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table_layout);
+    }
+}
