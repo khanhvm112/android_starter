5ca1cfa74f646918c770c1fa4b630250e4efb9b1
KHANHVM\KhanhVM <khanhvm112@gmail.com>
2022-07-27 11:52:40 +0700

    first commit

---
+++ app/src/main/java/com/example/javaproject/layout/LinearLayout.java
@@ -0,0 +1,16 @@
+package com.example.javaproject.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.javaproject.R;
+
+public class LinearLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout);
+    }
+}
---
+++ app/src/main/java/com/example/javaproject/layout/RelativeLayout.java
@@ -0,0 +1,16 @@
+package com.example.javaproject.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.javaproject.R;
+
+public class RelativeLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout);
+    }
+}
